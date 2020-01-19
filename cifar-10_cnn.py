7ae11acd74f0500c51640e1f31f26883ba064a71
laptop_local <lemeng1@qq.com>
2020-01-19 10:28:55 +0800

    添加图片

--- cifar-10_cnn.py
+++ cifar-10_cnn.py
@@ -1,27 +1,24 @@
-import keras
-from keras import Input, Model
-from keras.datasets import cifar10
-from keras.optimizers import RMSprop
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
 import os
 import shutil
+
+from keras import Input, Model
+from keras.datasets import cifar10
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from keras.optimizers import RMSprop
+from keras.utils import to_categorical, plot_model
 from matplotlib import pyplot as plt
 
-from keras.utils import plot_model
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # 指定使用哪个GPU进行训练
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
+epochs = 5  # 迭代次数
 batch_size = 32  # 批大小
-num_classes = 10  # 有多少个类别
-epochs = 1  # 迭代次数
-num_predictions = 20  # 预测多少个图片
-input_shape = (32, 32, 3)  # 输入图片的shape
 opt = RMSprop(lr=0.0001, decay=1e-6)  # 使用RMSprop优化器
+num_classes = 10  # 有多少个类别
+input_shape = (32, 32, 3)  # 图片的shape
 output_dir = './output'  # 输出目录
 if os.path.exists(output_dir):
     shutil.rmtree(output_dir)
-    print('%s文件夹已存在，没关系，删掉了' % output_dir)
+    print('%s文件夹已存在，但是没关系，我们删掉了' % output_dir)
 os.mkdir(output_dir)
 print('%s已创建' % output_dir)
 
@@ -31,8 +28,8 @@ x_train = x_train.astype('float32') / 255  # 归一化
 x_val = x_val.astype('float32') / 255
 
 # 将向量转化为二分类矩阵，也就是one-hot编码
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_val = keras.utils.to_categorical(y_val, num_classes)
+y_train = to_categorical(y_train, num_classes)
+y_val = to_categorical(y_val, num_classes)
 
 # 创建模型
 input = Input(shape=input_shape)
@@ -48,9 +45,9 @@ x = Flatten()(x)
 x = Dense(units=512, activation='relu')(x)
 x = Dense(units=num_classes, activation='softmax')(x)
 model = Model(input, x)  # 创建模型
-model.summary()  # 控制台打印模型参数信息
-model_img = output_dir + '/cifar10_cnn.png'  # 模型图片的保存路径
-plot_model(model, to_file=model_img, show_shapes=True)  # 模型参数信息保存为一张图片
+model.summary()  # 打印到控制台查看模型参数信息
+model_img = output_dir + '/cifar10_cnn.png'  # 模型结构图保存路径
+plot_model(model, to_file=model_img, show_shapes=True) # 模型结构保存为一张图片
 print('%s已保存' % model_img)
 
 # 编译模型
@@ -65,10 +62,11 @@ model.fit(x_train, y_train,
           validation_data=(x_val, y_val),  # 指定验证集
           shuffle=True)  # 混洗数据，也就是把数据打乱
 
-# 保存整个模型
+# 保存模型
 model_path = output_dir + '/keras_cifar10_trained_model.h5'
 model.save(model_path)
 print('%s已保存' % model_path)
+
 
 # 取验证集里面的图片拿来预测看看
 name = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer',
@@ -76,6 +74,7 @@ name = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer',
 n = 20  # 取多少张图片
 x_val = x_val[:n]
 y_val = y_val[:n]
+
 
 # 预测
 y_predict = model.predict(x_val, batch_size=n)
@@ -87,11 +86,12 @@ for i in range(n):
     plt.axis('off')  # 取消x,y轴坐标
     plt.imshow(x_val[i])  # 显示图片
     if y_val[i].argmax() == y_predict[i].argmax():
-        # 如果预测正确，绿色标题
+        # 预测正确，用绿色标题
         plt.title('%s,%s' % (name[y_val[i].argmax()], name[y_predict[i].argmax()]), color='green')
     else:
-        # 如果预测错误，红色标题
+        # 预测错误，用红色标题
         plt.title('%s,%s' % (name[y_val[i].argmax()], name[y_predict[i].argmax()]), color='red')
 plt.show()  # 显示画布
 predict_img = output_dir + '/predict.png'
+print('%s已保存' % predict_img)
 plt.savefig(predict_img)  # 保存预测图片
